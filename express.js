1aeecd3c04b9e9bd4f17d32745253fe9a6bb9187
Adrien <adriengledel@hotmail.fr>
2019-07-28 14:13:10 +0200

    /

--- express.js
+++ express.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 if(process.env.NODE_ENV === "production"){
   app.use(express.static("client/build"));
-  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  });
+  app.get('/', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  });
 }
 else{
   app.use(express.static("public"));
